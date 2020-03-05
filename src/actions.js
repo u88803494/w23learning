ea6877a2fe1cd18f39b9d7fadee1c4319404a13d
u88803494 <u88803494@gmail.com>
2020-03-05 14:02:23 +0800

    thunk 前置

--- src/actions.js
+++ src/actions.js
@@ -1,17 +1,26 @@
 import * as actionTypes from './actionTypes';
+import * as WebAPI from '../WebAPI';
 
 export const updateNavText = (text) => {
-	return {
-		type: actionTypes.UPDATE_NAV_TEXT,
-		value: text,
-	};
+  return {
+    type: actionTypes.UPDATE_NAV_TEXT,
+    value: text,
+  };
 };
 
 export const getPosts = () => ({
-	type: actionTypes.GET_POSTS,
+  type: actionTypes.GET_POSTS,
 });
 
 export const getPostsSuccess = (data) => ({
-	type: actionTypes.GET_POSTS_SUCCESS,
-	data,
+  type: actionTypes.GET_POSTS_SUCCESS,
+  data,
 });
+
+export const getPostList = (dispatch) => {
+  dispatch(getPosts())
+  WebAPI.getPosts()
+    .then(res => {
+      dispatch(getPostsSuccess(res.data))
+    })
+}
